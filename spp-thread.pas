ed9c5d76d42a31d9e9c6f983ae02beb24917c074
garbagememo <66831242+garbagememo@users.noreply.github.com>
2025-01-12 18:15:30 +0900

    コマンド版FreePascalのマルチスレッドで動く版
    ようやくマルチスレッドの初歩がわかった

--- spp-thread.pas
+++ spp-thread.pas
@@ -14,59 +14,69 @@ var
   BMP:BMPrecord;
 
 type
-  LineArray=array[0..255] of rgbColor;
+   
+  LineArray=array[0..255*255] of rgbColor;
   TMyThread = class(TThread)
+     samps:integer;//render option
      y:integer;
      Line:LineArray;
+     rt:TRenderClass;
      procedure Execute; override;
      procedure AddAxis;
   end;
   
   procedure TMyThread.Execute;
   var
-    i:byte;
+     x,sx,sy,s:integer;
+     r,tColor,temp:Vec3;
   begin
-     while y<255 do begin
-        for i:=0 to 255  do begin
-           Line[i].r:=i;
-           Line[i].g:=y;
-           Line[i].b:=128;
-        end;
-        Synchronize(@AddAxis);
-     end;
+    while y<rt.cam.h do begin
+      if y mod 10 =0 then writeln('y=',y);
+      for x := 0 to rt.cam.w - 1 do begin
+        r:=ZeroVec;
+        tColor:=ZeroVec;
+        for sy := 0 to 1 do begin
+          for sx := 0 to 1 do begin
+            for s := 0 to samps - 1 do begin
+              temp:=rt.Radiance(rt.cam.GetRay(x,y,sx,sy), 0);
+              temp:= temp/ samps;
+              r:= r+temp;
+            end;(*samps*)
+            temp:= ClampVector(r)* 0.25;
+            tColor:=tColor+ temp;
+            r:=ZeroVec;
+          end;(*sx*)
+        end;(*sy*)
+        Line[x]:=ColToRGB(tColor);
+      end;(* for x *)
+      Synchronize(@AddAxis);
+     end;(*for y*)
   end;
   procedure TMyThread.AddAxis;
   var
      j:integer;
+     yAxis:integer;
   begin
-     writeln(' y=',y);
-     for j:=0 to 255 do BMP.SetPixel(j,y,line[j]);
+     yAxis:=rt.cam.h-y-1;
+     for j:=0 to rt.cam.w-1 do BMP.SetPixel(j,yAxis,line[j]);
      y:=y+MaxThread;
   end;
   
   
 var
-  x,y,sx,sy,i,s: integer;
-  w,h,samps,height    : integer;
-  temp,d       : Vec3;
-  r1,r2,dx,dy  : real;
+  i: integer;
+  w,h,samps    : integer;
   cam:CamRecord;
-   cx,cy: Vec3;
-  tColor,r,camPosition,camDirection : Vec3;
+  camPosition,camDirection : Vec3;
 
-  ScrWidth,ScrHeight:integer;
-  vColor:rgbColor;
   ArgInt:integer;
   FN,ArgFN:string;
   c:char;
-
-  rt:TRenderClass;
-
 var
   ThreadAry:array[0..MaxThread-1] of TMyThread;
 begin
   FN:='temp.bmp';
-  w:=256 ;h:=192;  samps := 16;
+  w:=640 ;h:=480;  samps := 16;
   c:=#0;
   repeat
     c:=getopt('o:s:w:');
@@ -94,7 +104,6 @@ begin
       end;
     end; { case }
   until c=endofoptions;
-  height:=h;
   BMP.new(w,h);
   InitScene;
   Randomize;
@@ -104,53 +113,26 @@ begin
   camDirection:=camDirection.norm;
   cam.new(camPosition, camDirection,w,h );
 
-  rt:=TRenderClass.Create(sph,cam);
-
-  
-  ScrWidth:=0;
-  ScrHeight:=0;
-  writeln ('The time is : ',TimeToStr(Time));
-
-  for y := 0 to h-1 do begin
-    if y mod 10 =0 then writeln('y=',y);
-    for x := 0 to w - 1 do begin
-      r:=ZeroVec;
-      tColor:=ZeroVec;
-      for sy := 0 to 1 do begin
-        for sx := 0 to 1 do begin
-           for s := 0 to samps - 1 do begin
-              temp:=rt.Radiance(Cam.GetRay(x,y,sx,sy), 0);
-              temp:= temp/ samps;
-              r:= r+temp;
-          end;(*samps*)
-          temp:= ClampVector(r)* 0.25;
-          tColor:=tColor+ temp;
-          r:=ZeroVec;
-        end;(*sx*)
-      end;(*sy*)
-      vColor:=ColToRGB(tColor);
-      BMP.SetPixel(x,height-y,vColor);
-    end;(* for x *)
-  end;(*for y*)
-  writeln ('The time is : ',TimeToStr(Time));
-  BMP.WriteBMPFile(FN);
-
-
-
+   writeln ('The time is : ',TimeToStr(Time));
    
-  BMP.new(256,256);
+  BMP.new(cam.w,cam.h);
   for i:=0 to MaxThread-1 do begin
-    ThreadAry[i]:=TMyThread.Create(true);ThreadAry[i].FreeOnTerminate:=true;
-    ThreadAry[i].y:=i;
+     ThreadAry[i]:=TMyThread.Create(true);
+     ThreadAry[i].FreeOnTerminate:=false;//falseにしないとスレッドが休止時の後始末ができない。
+     ThreadAry[i].rt:=TRenderClass.Create(sph,cam);
+     ThreadAry[i].y:=i;
+     ThreadAry[i].samps:=samps;
   end;
   writeln('Setup!');
   
   for i:=0 to MaxThread-1 do begin
     ThreadAry[i].Start;
   end;
-   for i:=0 to MaxThread-1 do begin
+  //このルーチンが別途で無いとマルチスレッドにならない
+  for i:=0 to MaxThread-1 do begin
     ThreadAry[i].WaitFor;
-   end;
+  end;
+   writeln(' ober=');
    BMP.WritePPM('threadtest.ppm');
 end.
   
