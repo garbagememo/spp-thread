1237394462b3d359e3c36d3b5ba7f981f18542cf
garbagememo <66831242+garbagememo@users.noreply.github.com>
2025-01-24 23:50:25 +0900

    PNGフォーマット（8bit輝度）対応

--- sphybrid-thread.pas
+++ sphybrid-thread.pas
@@ -293,6 +293,11 @@ begin
   end;
   writeln('The time is : ',TimeToStr(Time));
   writeln('Calcurate time is=',TimeToStr(Time-StarTime));
-  if UpperCase(ExtractFileExt(FN))='.BMP' then  BMP.WriteBMPFile(FN) else BMP.WritePPM(FN);
+  if UpperCase(ExtractFileExt(FN))='.BMP' then
+    BMP.WriteBMPFile(FN)
+  else if UpperCase(ExtractFileExt(FN))='.PNG' then
+    BMP.WritePNG(FN)
+  else
+    BMP.WritePPM(FN);
 end.
   
